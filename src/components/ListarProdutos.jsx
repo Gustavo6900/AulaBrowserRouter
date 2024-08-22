ca6def2c235f62ebe752d34c28401108645021a9
Gustavo6900 <gustavopassianotocod@gmail.com>
2024-08-21 21:54:06 -0300

    teste

--- src/components/ListarProdutos.jsx
+++ src/components/ListarProdutos.jsx
@@ -1,15 +1,16 @@
 
-export default function ListarProdutos ( props){
+
+export default function ListarProdutos ( {props}){
     return(
 
         <div>
-          <p>ai</p>
+        
             {
                 props.map((produto)=>
                     <di key= {produto.id}>
                         <h1>{produto.title}</h1>
                         <h2>{produto.marca}</h2>
-                        <img src= {produto.img}/>
+                        <img src= {produto.imagem}/>
                         <p>{produto.cor}</p>
                         <p>{produto.preco}</p>
                     </di>
