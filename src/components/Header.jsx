36d6d1f75df3b725fe77530344412cdb0c1eebf1
Gustavo6900 <gustavopassianotocod@gmail.com>
2024-08-23 10:45:59 -0300

    CSS FEIO

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom"
 export default function Header (){
     return(
         <div className="nav">
+            <div className="nomeLoja">
+            <h1 >GP Imports</h1>
+            </div>
             <div className="opcoes">
                 <Link to="/" className="paginas" >Home</Link>
                 <Link to="/Produtos" className="paginas" >Produtos</Link>
                 <Link to="/Ofertas" className="paginas" >Ofertas</Link>
             </div>
-
-            <h1 className="nomeLoja">GP Imports</h1>
         </div>
     )
 }
